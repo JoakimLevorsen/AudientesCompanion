823d65e9d6b7fef3a45392b7ccfdeb223e555aee
Joakim Levorsen <joakim.levorsen@icloud.com>
2019-10-24 11:30:14 +0200

    initial commit

---
+++ app/src/main/java/com/a2electricboogaloo/audientes/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.a2electricboogaloo.audientes.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
