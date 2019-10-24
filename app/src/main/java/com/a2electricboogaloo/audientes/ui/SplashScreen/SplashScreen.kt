02470668b7eda18031fb0ec50234758b5f67ec68
theogutt <theodorguttesen@gmail.com>
2019-10-24 14:53:58 +0200

    splashscreen xml

---
+++ app/src/main/java/com/a2electricboogaloo/audientes/ui/SplashScreen/SplashScreen.kt
@@ -0,0 +1,13 @@
+package com.a2electricboogaloo.audientes.ui.SplashScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.a2electricboogaloo.audientes.R
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen2)
+    }
+}
