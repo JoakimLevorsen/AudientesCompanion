9dc584081c8abad9d026ecce41ac80e2f7ed37b8
Ida Christensen <idac1998@gmail.com>
2019-11-19 12:40:14 +0100

    Changed the buttons text color
    Deleted volume activity

--- app/src/main/java/com/a2electricboogaloo/audientes/ui/audiograms/AudiogramFragment.kt
+++ app/src/main/java/com/a2electricboogaloo/audientes/ui/audiograms/AudiogramFragment.kt
@@ -6,8 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ListView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -29,7 +27,7 @@ class AudiogramFragment : Fragment() {
         audiogramViewModel.text.observe(this, Observer {
             //textView.text = it
         })
-        val button = root.findViewById<Button>(R.id.button5)
+        val button = root.findViewById<Button>(R.id.addTest)
         val intent = Intent(this.context, HearingTest::class.java)
         button?.setOnClickListener { startActivity(intent) }
 
