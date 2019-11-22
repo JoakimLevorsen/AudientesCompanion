9e79f1170f101124e6c52e9741782d1909136463
SersanAslan <s185021@student.dtu.dk>
2019-11-22 12:48:58 +0100

    Navigationsbar i velkomstskærmen er fjernet, for at gøre det mere smooth :-)

--- app/src/main/java/com/a2electricboogaloo/audientes/model/bluetooth/DeviceScanActivity.kt
+++ app/src/main/java/com/a2electricboogaloo/audientes/model/bluetooth/DeviceScanActivity.kt
@@ -1,35 +1,5 @@
 package com.a2electricboogaloo.audientes.model.bluetooth
 
-import android.app.ListActivity
-import android.bluetooth.BluetoothAdapter
-import android.os.Handler
-
-private const val SCAN_PERIOD: Long = 10000
-
-
-class DeviceScanActivity (
-    private val bluetoothAdapter: BluetoothAdapter,
-    private val handler: Handler
-) : ListActivity() {
-
-    private var mScanning: Boolean = false
-
-    private fun scanLeDevice(enable: Boolean) {
-        when (enable) {
-            true -> {
-                // Stops scanning after a pre-defined scan period.
-                handler.postDelayed({
-                    mScanning = false
-                    //  bluetoothAdapter.stopLeScan(leScanCallback)
-                }, SCAN_PERIOD)
-                mScanning = true
-                // bluetoothAdapter.startLeScan(leScanCallback)
-            }
-            else -> {
-                mScanning = false
-                //  bluetoothAdapter.stopLeScan(leScanCallback)
-            }
-        }
-    }
-
+class DeviceScanActivity() {
+    
 }
--- app/src/main/java/com/a2electricboogaloo/audientes/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/a2electricboogaloo/audientes/ui/welcome/WelcomeActivity.kt
@@ -2,21 +2,17 @@ package com.a2electricboogaloo.audientes.ui.welcome
 
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
-import android.bluetooth.BluetoothManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.a2electricboogaloo.audientes.MainActivity
 import com.a2electricboogaloo.audientes.R
-import java.util.ArrayList
 
 
 class WelcomeActivity : AppCompatActivity() {
@@ -25,39 +21,18 @@ class WelcomeActivity : AppCompatActivity() {
     private var nextButton: Button? = null
     private var titleText: TextView? = null
     private var contentText: TextView? = null
-   // private fun PackageManager.missingSystemFeature(name: String): Boolean = !hasSystemFeature(name)
-   // private val bluetoothAdapter: BluetoothAdapter? by lazy(LazyThreadSafetyMode.NONE) {
-    //    val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-     //   bluetoothManager.adapter
-    //}
-  //  private val BluetoothAdapter.isDisabled: Boolean
-    //    get() = !isEnabled
-
-
-    val REQUEST_ENABLE_BT = 1
+    private val REQUEST_ENABLE_BT = 1
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        hideNavBar()
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
-        hideNavBar()
 
         nextButton = findViewById<Button>(R.id.button11)
         titleText = findViewById<TextView>(R.id.titleView)
         contentText = findViewById<TextView>(R.id.contentView)
-
-       /* packageManager.takeIf { it.missingSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE) }
-            ?.also {
-                Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show()
-            }
-       bluetoothAdapter?.takeIf { it.isDisabled }?.apply {
-            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            val REQUEST_ENABLE_BT = 1
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
-        }
-        */
-
 
         nextButton?.setOnClickListener {
             activateBT()
@@ -111,7 +86,8 @@ class WelcomeActivity : AppCompatActivity() {
     }
 
     private fun hideNavBar() {
-        this.window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        this.window.decorView.systemUiVisibility =
+            (      View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
