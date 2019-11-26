31ffdbae1f5958eff64d54b14cc3bab67c756be6
Ida Christensen <idac1998@gmail.com>
2019-11-26 11:54:01 +0100

    Tilføjet noget kode, og ændret lidt i den kode der allerede var der.
    Bluetooth connect virker ikke endnu.
    device.address skal der også findes en løsning til.

--- app/src/main/java/com/a2electricboogaloo/audientes/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/a2electricboogaloo/audientes/ui/welcome/WelcomeActivity.kt
@@ -3,12 +3,17 @@ package com.a2electricboogaloo.audientes.ui.welcome
 import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.AsyncTask
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.a2electricboogaloo.audientes.MainActivity
 import com.a2electricboogaloo.audientes.R
 import kotlinx.android.synthetic.main.activity_welcome.*
+import java.io.IOException
 import java.util.*
 
 /**
@@ -26,6 +31,11 @@ class WelcomeActivity : AppCompatActivity() {
     private var stringArrayList: ArrayList<String>? = null
     private var arrayAdapter: ArrayAdapter<String>? = null
 
+    var bluetoothSocket: BluetoothSocket? = null
+    var isConnected: Boolean = false
+    var AUDIENTES_UUID_ROOT: UUID = UUID.fromString("e2190000-ec7a-45d0-9ecf-6ccfdee39a27")
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
@@ -41,6 +51,7 @@ class WelcomeActivity : AppCompatActivity() {
         btEnablingIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
         requestCodeForBT = 1
 
+        showListOfBondedDevices()
         bluetoothActivate()
     }
 
@@ -55,10 +66,16 @@ class WelcomeActivity : AppCompatActivity() {
                     "Bluetooth does not support this device",
                     Toast.LENGTH_LONG
                 ).show()
+
+            } else if (!bluetoothAdapter.isEnabled) {
+                startActivityForResult(btEnablingIntent, requestCodeForBT!!)
+
             } else {
-                if (!bluetoothAdapter.isEnabled) {
-                    startActivityForResult(btEnablingIntent, requestCodeForBT!!)
-                }
+                ConnectToDevice(this).execute()
+                val intent = Intent(this, MainActivity::class.java)
+                val lambda = { -> startActivity(intent) }
+                finish()
+                lambda()
             }
         }
     }
@@ -73,7 +90,7 @@ class WelcomeActivity : AppCompatActivity() {
         var arrayBT = arrayOfNulls<String>(setBT.size)
         var index = 0
 
-        if (setBT.size > 0) {
+        if (setBT.isNotEmpty()) {
             for (device: BluetoothDevice in setBT) {
                 arrayBT[index] = device.name
                 index++
@@ -91,6 +108,8 @@ class WelcomeActivity : AppCompatActivity() {
         if (requestCode == requestCodeForBT) {
             if (resultCode == Activity.RESULT_OK) {
                 Toast.makeText(applicationContext, "Bluetooth is enabled", Toast.LENGTH_LONG).show()
+                showListOfBondedDevices()
+                ConnectToDevice(this).execute()
             } else if (resultCode == Activity.RESULT_CANCELED) {
                 Toast.makeText(applicationContext, "Bluetooth is canceled", Toast.LENGTH_LONG)
                     .show()
@@ -98,5 +117,43 @@ class WelcomeActivity : AppCompatActivity() {
         }
     }
 
+   private inner class ConnectToDevice(c: Context) : AsyncTask<Void, Void, String>() {
+        private var connectSuccess: Boolean = true
+        private val context: Context
 
+        init {
+            this.context = c
+        }
+
+        override fun doInBackground(vararg params: Void?): String? {
+            try {
+                if (bluetoothSocket == null || !isConnected){
+                    bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+                    val device: BluetoothDevice = bluetoothAdapter.getRemoteDevice(bluetoothAdapter.address) //wrong address, it needs 'device.address' from bondedDevices
+                    bluetoothSocket = device.createInsecureRfcommSocketToServiceRecord(AUDIENTES_UUID_ROOT)
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery()
+
+                    bluetoothSocket?.connect() //It does not connect yet
+                }
+            } catch (e: IOException){
+                connectSuccess = false
+                e.printStackTrace()
+            }
+            return null
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            if (!connectSuccess) {
+                println("could not connect")
+            } else {
+                isConnected = true
+                println("connected")
+            }
+        }
+    }
 }
+
+
+
+
