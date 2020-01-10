2851c17bd24eed4e6e38663072675dc44a1b9428
TMJ97 <s185027@student.dtu.dk>
2020-01-10 14:54:02 +0100

    Seekbar Overall Volume

--- app/src/main/java/com/a2electricboogaloo/audientes/ui/home/HomeFragment.kt
+++ app/src/main/java/com/a2electricboogaloo/audientes/ui/home/HomeFragment.kt
@@ -6,12 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.SeekBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.a2electricboogaloo.audientes.R
 import com.a2electricboogaloo.audientes.ui.hearing.HearingTest
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -33,6 +37,31 @@ class HomeFragment : Fragment() {
         val button = root.findViewById<Button>(R.id.goButton)
         val intent = Intent(this.context, HearingTest::class.java)
         button?.setOnClickListener { startActivity(intent) }
+
+        val seekBarOverall = root.findViewById<SeekBar>(R.id.sliderOverall)!!
+        seekBarOverall.max = 200
+        seekBarOverall.progress = 100 //Default progress value
+
+
+
+        seekBarOverall.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBarOverall: SeekBar,
+                                           progress: Int, fromUser: Boolean) {
+                //TODO write custom code for progress is changed
+            }
+
+            override fun onStartTrackingTouch(seekBarOverall: SeekBar) {
+                //TODO write custom code for progress is started
+            }
+
+            override fun onStopTrackingTouch(seekBarOverall: SeekBar) {
+                //TODO write custom code for progress is stopped
+                val snackyText = Snackbar.make(view!!, "Volume is: ${seekBarOverall.progress / 10}", Snackbar.LENGTH_SHORT)
+                snackyText.show()
+            }
+        })
+
 
         return root
     }
