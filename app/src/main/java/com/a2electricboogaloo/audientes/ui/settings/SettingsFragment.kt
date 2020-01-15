588fac7dbaf59107288a96d1775d6985dd1b1dd1
SersanAslan <s185021@student.dtu.dk>
2020-01-15 14:08:46 +0100

    Fixed crashing

--- app/src/main/java/com/a2electricboogaloo/audientes/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/a2electricboogaloo/audientes/ui/settings/SettingsFragment.kt
@@ -4,12 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.a2electricboogaloo.audientes.R
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 
 
 class SettingsFragment : Fragment() {
@@ -28,17 +26,5 @@ class SettingsFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_settings, container, false)
         return root
 
-        val buttonSignIn = root.findViewById<Button>(R.id.button_signIn)
-        buttonSignIn.setOnClickListener{
-
-                  }
-        val buttonSignUp = root.findViewById<Button>(R.id.button_signUp)
-        buttonSignIn.setOnClickListener{
-
-        }
-        val buttonSignOut = root.findViewById<Button>(R.id.button_signOut)
-        buttonSignIn.setOnClickListener{
-
-        }
     }
 }
