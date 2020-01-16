5fad6293373b56b548b91ebe4e73f56e91b597ac
theogutt <theodorguttesen@gmail.com>
2020-01-16 13:00:44 +0100

    authentication 2

--- app/src/main/java/com/a2electricboogaloo/audientes/ui/login/SignUpActivity.kt
+++ app/src/main/java/com/a2electricboogaloo/audientes/ui/login/SignUpActivity.kt
@@ -1,13 +1,46 @@
 package com.a2electricboogaloo.audientes.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.a2electricboogaloo.audientes.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import kotlinx.android.synthetic.main.activity_signin.*
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        auth = FirebaseAuth.getInstance()
+
+        buttonSignUpDone.setOnClickListener{
+            signUp(
+                edittext_emailSignUp!!
+                    .text.toString(),
+                edittext_passwordSignUp!!.text.toString()
+            )
+        }
+
+    }
+    fun signUp(email : String, password : String){
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("login", "createUserWithEmail:success")
+                    val user = auth.currentUser
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("login", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
