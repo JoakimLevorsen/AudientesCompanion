d22360ecae3f8210837dcfc09200b7311d0b0998
Ida Christensen <idac1998@gmail.com>
2020-01-21 11:27:27 +0100

    Audio giver 0

--- app/src/main/java/com/a2electricboogaloo/audientes/ui/hearing/HearingTest.kt
+++ app/src/main/java/com/a2electricboogaloo/audientes/ui/hearing/HearingTest.kt
@@ -1,18 +1,13 @@
 package com.a2electricboogaloo.audientes.ui.hearing
 
 import android.Manifest
-import android.content.DialogInterface
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.media.MediaRecorder
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.a2electricboogaloo.audientes.R
-
 
 
 
@@ -25,12 +20,9 @@ class HearingTest : AppCompatActivity() {
         setContentView(R.layout.activity_hearing_test)
 
         val button = findViewById<Button>(R.id.startButton3)
-
         checkPermission()
 
-
         button.setOnClickListener {
-            stop()
             getSound()
         }
     }
@@ -52,7 +44,7 @@ class HearingTest : AppCompatActivity() {
    }
 
    fun checkEnvironmentNoise() {
-       if (audio == null) {
+           audio = MediaRecorder()
            audio?.setAudioSource(MediaRecorder.AudioSource.MIC)
            audio?.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
            audio?.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
@@ -60,62 +52,19 @@ class HearingTest : AppCompatActivity() {
            audio?.prepare()
            audio?.start()
 
-            println("checking sound level")
-       } else println("no audio input")
-/*
-      else if (audio != null) {
-
-       } else {
-           println("NO AUDIO INPUT")
-       }
-
- */
-
-       //audio?.startRecording()
-
-/*
-       if (audio == null) {
-           Toast.makeText(this, "DER ER INGEN LYD, du kan godt tage en test nu", Toast.LENGTH_LONG)
-               .show()
-           audio?.stop()
-           println("HER ER RESULTATET" + audio?.release())
-
-       } else if (audio != null) {
-           Toast.makeText(this, "du kan IKKE tage en test nu", Toast.LENGTH_LONG).show()
-
-           println("RES FRA MEDIArecorder" + audio?.maxAmplitude)
-
-           // val intent = Intent(this, MainActivity::class.java)
-           //startActivity(intent)
-
-           audio?.stop()
-           // println( "HER ER DET 2. RESULTAT " + audio?.release())
-       }
-
- */
+           println("sound level is:  " + audio?.maxAmplitude)
    }
 
-    fun stop(){
-
-            audio?.stop()
-            audio?.release()
-            audio = null
-            println("stopping recorder")
-
-    }
-
     fun getSound(){
-
             audio?.maxAmplitude
             println("max AMPLITUDE for AUDIO: " + audio?.maxAmplitude)
-
+           // startHearingTest()
     }
 
     fun startHearingTest(){
-        val fragment = HearingTest_complete_frag()
+        val fragment = HearingTest_running_frag()
         val transaction = getSupportFragmentManager().beginTransaction()
         transaction.add(R.id.fragmentindhold, fragment)
         transaction.commit()
     }
- 
 }
