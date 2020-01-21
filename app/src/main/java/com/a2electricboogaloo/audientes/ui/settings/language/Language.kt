07b0c51f57197e7ec96ded77243536c0091b4d76
SersanAslan <s185021@student.dtu.dk>
2020-01-21 11:42:08 +0100

    added new branch for the language section under the settings and implemented some of them.

---
+++ app/src/main/java/com/a2electricboogaloo/audientes/ui/settings/language/Language.kt
@@ -0,0 +1,23 @@
+package com.a2electricboogaloo.audientes.ui.settings.language
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.LinearLayout
+import com.a2electricboogaloo.audientes.R
+
+class Language : Activity() {
+
+    private lateinit var uk_english: LinearLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.language_activity)
+
+        uk_english = findViewById(R.id.uk_flag)
+
+    }
+
+}
+
+
+
